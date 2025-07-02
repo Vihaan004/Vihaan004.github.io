5d34cb4ce7d707181a4d4c3d7cbf8d7a0a9c892e
Vihaan <vihaan004@gmail.com>
2025-07-02 08:10:33 -0700

    links, music

--- src/components/Music.jsx
+++ src/components/Music.jsx
@@ -5,15 +5,15 @@ function Music() {
     <div className="Music content-width">
       <div className="music-tag">
         <h1>Music</h1>
+        <p className="music-intro">Some of my favorites...</p>
       </div>
       <div className="music-content">
-        <p className="music-intro">Here's what I've been listening to lately:</p>
         <div className="playlist-container">
           <iframe 
             style={{borderRadius:"12px"}} 
             src="https://open.spotify.com/embed/playlist/1LUv8lCss48NDhSBaxTHry?utm_source=generator&theme=0" 
             width="100%" 
-            height="352" 
+            height="152" 
             frameBorder="0" 
             allowFullScreen="" 
             allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" 
