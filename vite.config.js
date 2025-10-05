991a1de891bce1ff175890120f4474bb2ffdba27
Vihaan <vihaan004@gmail.com>
2025-10-05 16:03:11 -0700

    deploy workflow

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/',
+  base: '/', // Use '/' since you have a custom domain
   build: {
     outDir: 'docs'
   },
