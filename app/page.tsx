703a13bf0034e12e62adcfbe0be45b07c6025af9
Vihaan <vihaan004@gmail.com>
2026-02-23 23:42:31 -0700

    update font

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,12 @@
 export default function Home() {
   return (
     <main>
-      <p>Hello! 👋🏽</p>
-      <p>I'm Vihaan. I'm a computer systems engineering student at Arizona State University. <br />I like building systems that make chaos look intentional,  blending hardware, software, and a questionable amount of curiosity.</p>
+      <p>Hello! 👋🏽 I'm Vihaan.</p>
+      <p>I'm a computer systems engineering student at Arizona State University. <br />I like building systems that make chaos look intentional,  blending hardware, software, and a questionable amount of curiosity.</p>
       <p>Currently working on:</p>
       <ul>
-        <li><a href="/blog/agentic-ai-in-dv">Agentic framework for hardware design verification</a></li>
-        <li><a href="/blog/collagent">Collagent - the agent for college students</a></li>
+        <li><a href="/blog/agentic-ai-in-dv">An agentic framework for coverage closure in design verification</a></li>
+        <li><a href="/blog/collagent">Collagent - for students like me</a></li>
       </ul>
       <p></p>
     </main>
