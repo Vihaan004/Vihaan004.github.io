e4e3a2c427619301798c628626209209a02e0495
Vihaan <vihaan004@gmail.com>
2026-07-19 20:10:03 -0700

    minor updates

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <main>
       <p>Hello! 👋🏽 I'm Vihaan.</p>
-      <p>Hardware DV by trade, automation by habit.</p>
+      <p>Hardware design verification by trade, automation by habbit.</p>
       <p>I'm a recent CompEng grad based in the US.<br/>Background in <i><b>hardware design verification and GPU acceleration</b></i>, now researching and building agentic tooling and LLM infra.</p>
       <p>Currently working as a research intern at <a href="https://labs.engineering.asu.edu/advent/themes/">The Advent Lab</a>, building <a href="/blog/agentic-ai-in-dv">CovAgent</a> - a stimulus-generation agent for coverage closure in hardware design verification.</p>
     </main>
